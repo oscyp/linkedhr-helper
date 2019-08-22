54a88775a0b8ba6c83e5369519df4f29e5595453
Patryk Domasik <patryk.domasik@outlook.com>
2019-08-23 00:35:35 +0200

    RC, bubles and box works

--- name-helper.js
+++ name-helper.js
@@ -1,107 +1,84 @@
-try{
-const establishContactCallback = function(mutationsList, observer) {
-    onEstablishContact();
-};
 
-const addNoteCallback = function(mutationsList, observer) {
+const establishContactHandler = function(){
+    const actionsPanel = document.getElementsByClassName("send-invite__actions")[0];
+
+    if (actionsPanel != null){
+        addNoteHandler();
+    }
+}
+
+const addNoteHandler = function(mutationsList, observer) {
     const cancelNode = document.getElementsByClassName("artdeco-button artdeco-button--3 artdeco-button--muted artdeco-button--secondary mr1")[0];
     const addNoteNode = document.getElementsByClassName("artdeco-button artdeco-button--secondary artdeco-button--3 mr1")[0];
 
     if (cancelNode != null){
-        cancelNode.onclick = onEstablishContact;
+        cancelNode.onclick = establishContactHandler;
     }
 
     if (addNoteNode != null){
-        onAddNote();
+        const sendButton = document.getElementsByClassName("artdeco-button artdeco-button--3 ml1")[0];
+
+        if (sendButton != null){
+            sendButton.onclick = onIndirectSendClick;
+        }
     }
 }
 
-const messageNodeCallback = function(mutationsList, observer){
+const messageHandler = function(mutationsList, observer){
     const sendButton = document.getElementsByClassName("msg-form__send-button artdeco-button artdeco-button--1")[0];
-    sendButton.onclick = onDirectSendClick;
-}
-
-const appoutletNodeCallback = function(mutationsList, observer){
-    const bubbleNode = document.getElementById("msg-overlay");
-    const authenticationNode = document.getElementsByClassName("authentication-outlet")[0];
-
-    if (bubbleNode != null){
-        bubbleObserver.observe(bubbleNode, bubbleConfig);
-    }
-
-    if (authenticationNode != null){
-        authenticationObserver.observe(authenticationNode, authenticationConfig);
-    }
-}
-
-const bubbleNodeCallback = function(mutationsList, observer){
-    const bubbles = document.querySelectorAll('[id^="msg-overlay-conversation-bubble-"]');
-    bubbles.forEach(addSendEvent);
-}
-
-const authenticationNodeCallback = function(mutationsList, observer){
-    const sendButton = document.getElementsByClassName("msg-form__send-button artdeco-button artdeco-button--1")[0];
-
+    
     if (sendButton != null){
-        const message = document.getElementsByClassName("msg-form__contenteditable t-14 t-black--light t-normal flex-grow-1 notranslate")[0].innerText;
-        const name = document.getElementsByClassName("msg-entity-lockup__entity-title truncate hoverable-link-text")[0].innerText;
-        sendButton.onclick = function(e){ onSendClick(e, message, name)};
+        sendButton.onclick = onDirectSendClick;
     }
 }
 
-const establishObserver = new MutationObserver(establishContactCallback);
-const actionsPanelObserver = new MutationObserver(addNoteCallback);
-const messageObserver = new MutationObserver(messageNodeCallback);
-const appoutletObserver = new MutationObserver(appoutletNodeCallback);
-const bubbleObserver = new MutationObserver(bubbleNodeCallback);
-const authenticationObserver = new MutationObserver(authenticationNodeCallback);
+const appoutletHandler = function(mutationsList, observer){
+    const bubbles = document.querySelectorAll('[id^="msg-overlay-conversation-bubble-"]');
+    const sendButton = document.getElementsByClassName("msg-form__send-button artdeco-button artdeco-button--1")[0];
 
-const establishConfig = { attributes: false, childList: true, subtree: false };
-const messageConfig = { attributes: true, childList: false, subtree: false };
-const actionsConfig = { attributes: false, childList: true, subtree: false };
-const appoutletObserverConfig = { attributes: false, childList: true, subtree: false };
-const bubbleConfig = { attributes: true, childList: true, subtree: true };
-const authenticationConfig = { attributes: false, childList: true, subtree: false };
+    if (bubbles != null){
+        bubbles.forEach(addSendEvent);
+    }
+    else if (sendButton != null){ //prevent overwriting
+        sendButton.onclick = onMessageBoxSendClick;
+    }
+}
 
 var obs = new MutationObserver(function (mutations, observer) {
-    onReady();
+    onBodyChange();
 });
 obs.observe(document.body, { childList: true, subtree: true, attributes: false, characterData: false });
 
-function onReady() {
+function onBodyChange() {
     const establishNode = document.getElementById("li-modal-container");
     const messageNode = document.getElementsByClassName("pv-s-profile-actions pv-s-profile-actions--send-in-mail ml2 artdeco-button artdeco-button--2 artdeco-button--secondary ember-view")[0];
     const appoutletNode = document.getElementsByClassName("application-outlet")[0];
 
     if (establishNode != null){
-        establishObserver.observe(establishNode, establishConfig);
+        establishContactHandler();
     }
 
     if (messageNode != null){
-        messageObserver.observe(messageNode, messageConfig);
+        messageHandler();
     }
 
     if (appoutletNode != null){
-        appoutletObserver.observe(appoutletNode, appoutletObserverConfig);
+        appoutletHandler();
     }
 }
 
 function addSendEvent(item, idx){
-    const name = item.getElementsByClassName("msg-overlay-bubble-header__primary-text t-14 t-black t-bold hoverable-link-text")[0].innerText;
-    const message = item.getElementsByClassName("msg-form__contenteditable t-14 t-black--light t-normal flex-grow-1 notranslate")[0].innerText;
     const sendButton = item.getElementsByClassName("msg-form__send-button artdeco-button artdeco-button--1")[0];
 
-    sendButton.onclick = function(e){ onSendClick(e, message, name)};
+    if (sendButton != null){
+        sendButton.onclick = function(e){ onBubbleSendClick(e, item) };
+    }
 }
 
-function onEstablishContact(){
-    const actionsPanel = document.getElementsByClassName("send-invite__actions")[0];
-    actionsPanelObserver.observe(actionsPanel, actionsConfig);
-}
-
-function onAddNote(){
-    const sendButton = document.getElementsByClassName("artdeco-button artdeco-button--3 ml1")[0];
-    sendButton.onclick = onIndirectSendClick;
+function onBubbleSendClick(e, item){
+    const name = item.getElementsByClassName("msg-overlay-bubble-header__primary-text t-14 t-black t-bold hoverable-link-text")[0].innerText;
+    const message = item.getElementsByClassName("msg-form__contenteditable t-14 t-black--light t-normal flex-grow-1 notranslate")[0].innerText;
+    onSendClick(e, message, name);
 }
 
 function onIndirectSendClick(e){
@@ -112,6 +89,12 @@ function onIndirectSendClick(e){
 function onDirectSendClick(e){
     const messageText = document.getElementsByClassName("msg-form__contenteditable t-14 t-black--light t-normal flex-grow-1 notranslate")[0].innerText;
     onSendClick(e, messageText, getName());
+}
+
+function onMessageBoxSendClick(e){
+    const message = document.getElementsByClassName("msg-form__contenteditable t-14 t-black--light t-normal flex-grow-1 notranslate")[0].innerText;
+    const name = document.getElementsByClassName("msg-entity-lockup__entity-title truncate hoverable-link-text")[0].innerText;  
+    onSendClick(e, message, name)
 }
 
 function onSendClick(e, message, name) {
@@ -142,9 +125,4 @@ function checkFirstLineNaming(message, name){
 function getName() {
     const nameText = document.getElementsByClassName("inline t-24 t-black t-normal break-words")[0].innerText;
     return nameText;
-}
-
-}
-catch(error){
-    console.error(error);
 }
